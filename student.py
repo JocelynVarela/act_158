cde09c4954192a6fe49fdede5865a33b9ca610f0
JocelynVarela <jocelyn2460@icloud.com>
2026-02-25 13:23:53 -0500

    Add files via upload

--- student.py
+++ student.py
@@ -6,9 +6,12 @@ class Student:
         self.bio_score = 0
         self.grade = 0
         self.cte = False
-        self.cert = False 
+        self.cert = False
+        self.ap = False
+        self.psat = 0
+        self.sat = 0 
         self.result1 = ["Please go enter your information to get pathway 1 results."]
         self.result2 = ["Please go enter your information to get pathway 2 results."]
         self.result3 = ["Please go enter your information to get pathway 3 results."]
         self.result4 = ["Please go enter your information to get pathway 4 results."]
-        self.result5 = ["Please go enter your information to get pathway 5 results."]
+        self.result5 = ["Please talk to your counselors to get you set up for pathway 5. It is important to know that in order to complete this pathway there are task they will advise you to complete."]
